9935a5dec45cc1e56e267e4b640515a54aa07dfa
Selvamurugan Senthilkumar <97260536+Selva0309@users.noreply.github.com>
2024-11-23 23:03:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const age = parseInt(document.getElementById('age').value);
+    const name = document.getElementById('name').value;
+
+    if (!age || !name) {
+        alert('Please fill out all fields');
+        return;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age >= 18) {
+                alert('Welcome, ' + name + '. You can vote.');
+                resolve();
+            } else {
+                alert('Oh sorry ' + name + '. You aren\'t old enough.');
+                reject();
+            }
+        }, 4000);
+    });
+
+    promise.then(() => {
+        // Code to execute on successful form submission
+    }).catch(() => {
+        // Code to execute if form submission fails
+    });
+});
